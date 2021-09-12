304eada35dbd239d3468b5f27adc59fd44a90aa7
Jarek <jareksedy@icloud.com>
2021-09-12 12:01:02 +0600

    Simplified weather model.

--- Elseweather/Global/Global.swift
+++ Elseweather/Global/Global.swift
@@ -8,3 +8,5 @@
 import Foundation
 
 typealias GeoLocation = (lat: Double, lon: Double)
+
+let apiKey: String = "f9979cbf228a4fa2a0e180440212605"
---
+++ Elseweather/Models/WeatherModel.swift
@@ -0,0 +1,78 @@
+//
+//  WeatherModel.swift
+//  Elseweather
+//
+//  Created by Ярослав on 12.09.2021.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    let location: Location
+    let current: Current
+}
+
+struct Location: Codable {
+    let name, region, country: String
+    let lat, lon: Double
+    let tzID: String
+    let localtimeEpoch: Int
+    let localtime: String
+
+    enum CodingKeys: String, CodingKey {
+        case name, region, country, lat, lon
+        case tzID = "tz_id"
+        case localtimeEpoch = "localtime_epoch"
+        case localtime
+    }
+}
+
+struct Current: Codable {
+    let lastUpdatedEpoch: Int
+    let lastUpdated: String
+    let tempC: Double
+    let tempF: Double
+    let isDay: Int
+    let condition: Condition
+    let windMPH: Double
+    let windKPH, windDegree: Int
+    let windDir: String
+    let pressureMB: Int
+    let pressureIN, precipMM, precipIN: Double
+    let humidity, cloud: Int
+    let feelslikeC, feelslikeF: Double
+    let visKM, visMiles, uv: Int
+    let gustMPH, gustKPH: Double
+    let airQuality: [String: Double]
+
+    enum CodingKeys: String, CodingKey {
+        case lastUpdatedEpoch = "last_updated_epoch"
+        case lastUpdated = "last_updated"
+        case tempC = "temp_c"
+        case tempF = "temp_f"
+        case isDay = "is_day"
+        case condition
+        case windMPH = "wind_mph"
+        case windKPH = "wind_kph"
+        case windDegree = "wind_degree"
+        case windDir = "wind_dir"
+        case pressureMB = "pressure_mb"
+        case pressureIN = "pressure_in"
+        case precipMM = "precip_mm"
+        case precipIN = "precip_in"
+        case humidity, cloud
+        case feelslikeC = "feelslike_c"
+        case feelslikeF = "feelslike_f"
+        case visKM = "vis_km"
+        case visMiles = "vis_miles"
+        case uv
+        case gustMPH = "gust_mph"
+        case gustKPH = "gust_kph"
+        case airQuality = "air_quality"
+    }
+}
+
+struct Condition: Codable {
+    let text, icon: String
+    let code: Int
+}
