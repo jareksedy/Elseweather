0ab96ccea8f2d0f2e820c99208cf7732b67046f1
Jarek <jareksedy@icloud.com>
2021-09-12 09:17:34 +0600

    Initial Commit

---
+++ Elseweather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Elseweather
+//
+//  Created by Ярослав on 12.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Elseweather/ElseweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElseweatherApp.swift
+//  Elseweather
+//
+//  Created by Ярослав on 12.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ElseweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
